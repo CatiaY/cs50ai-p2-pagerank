715c6a7aba371adbed30313f13fd5686b417959c
Catia Yamamura <cayamamura@gmail.com>
2025-10-02 21:42:03 -0300

    Add pagerank Random Surfer Model and Iterative algorithm

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# cs50ai-p2-pagerank
-Project 2 - PageRank: Uncertainty Problem - CS50’s Introduction to AI with Python
+# CS50’s Introduction to AI with Python
+
+This project was completed as part of the course assignments.  
+The following description is adapted from the original project specification.  
+
+## Project 2: PageRank (Lecture 2 – Uncertainty)  
+
+This project uses the PageRank’s algorithm to rank web pages by importance. 
+
+To approximate this ranking, two approaches are implemented:
+
+The first is the **Random Surfer Model**, which simulates a user navigating the web by randomly following links or occasionally jumping to a random page, controlled by a damping factor. Over many iterations, the frequency of visits to each page provides an estimate of its rank.
+
+The second approach uses the **Iterative algorithm**, applying a mathematical formula to repeatedly update PageRank values until they converge. 
+
+
+**Notes**
+- The description above is adapted from the official project specification.
+- Implemented using the course’s starter code, with modifications made to fulfill the project requirements.
+- Completed as part of the CS50’s Introduction to AI with Python (2024 edition) coursework.
+
+
+### How to Run
+
+1. Run the program using the command `python pagerank.py [corpus]`, where `[corpus]` is the name of the directory containing the web pages to analyze and compute PageRanks for.
--- pagerank.py
+++ pagerank.py
@@ -57,7 +57,33 @@ def transition_model(corpus, page, damping_factor):
     linked to by `page`. With probability `1 - damping_factor`, choose
     a link at random chosen from all pages in the corpus.
     """
-    raise NotImplementedError
+
+    linked_pages = corpus[page]    
+
+    # If page has no outgoing links, then transition_model 
+    # should return a probability distribution that 
+    # chooses randomly among all pages with equal probability. 
+    if len(linked_pages) == 0:
+        prob = 1 / len(corpus)
+        prob_dist = {key: prob for key in corpus}
+        return prob_dist
+
+
+    prob_dist = {key: 0 for key in corpus}
+
+    # With probability `damping_factor`, 
+    # choose a link at random linked to by `page`
+    link_prob = damping_factor / len(linked_pages)
+    for lp in linked_pages:
+        prob_dist[lp] += link_prob
+
+    # With probability `1 - damping_factor`, choose a 
+    # link at random chosen from all pages in the corpus.
+    page_prob = (1 - damping_factor) / len(corpus)
+    for key_page in prob_dist:
+        prob_dist[key_page] += page_prob
+    
+    return prob_dist
 
 
 def sample_pagerank(corpus, damping_factor, n):
@@ -69,7 +95,25 @@ def sample_pagerank(corpus, damping_factor, n):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+    
+    pagerank = {key: 0 for key in corpus}
+    
+    # First sample: randomly chosen page    
+    page = random.choice(list(corpus.keys()))
+    pagerank[page] += 1
+
+    for _ in range(n - 1):
+        prob_dist = transition_model(corpus, page, damping_factor)
+        page = random.choices(
+            population = list(prob_dist.keys()),
+            weights = list(prob_dist.values()),
+            k = 1
+        )[0]
+        pagerank[page] += 1
+
+    # Get probabilities
+    pagerank_prob = {key: value / n for key, value in pagerank.items()}
+    return pagerank_prob
 
 
 def iterate_pagerank(corpus, damping_factor):
@@ -81,7 +125,52 @@ def iterate_pagerank(corpus, damping_factor):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+    
+    n = len(corpus)
+    pagerank = {key: 1 / n for key in corpus}
+
+    convergence = 0.001
+
+    while True:
+        max_covergence = 0
+        new_pagerank= {}
+
+        # Rank of pages without links
+        rank_no_link = 0
+        for page, links in corpus.items():
+            if len(links) == 0:
+                rank_no_link += pagerank[page] / n
+
+        for page in corpus:
+            # Add page rank
+            new_rank = (1 - damping_factor) / n
+
+            # Add rank of pages without links 
+            new_rank += damping_factor * rank_no_link
+
+            # Add rank of pages i
+            sum_page_i = 0
+            for page_i, page_i_links in corpus.items():
+                if page in page_i_links:
+                    n_links = len(page_i_links)
+                    sum_page_i += pagerank[page_i] / n_links            
+            
+            new_rank += damping_factor * sum_page_i
+
+            # Compare old rank with new rank
+            change = abs(new_rank - pagerank[page])
+            max_covergence = max(max_covergence, change)
+            new_pagerank[page] = new_rank
+
+        pagerank = new_pagerank
+
+        if max_covergence < convergence:
+            break
+
+    # Normalize probabilities to sum = 1
+    s = sum(pagerank.values())
+    pagerank_normalized = {key: value / s for key, value in pagerank.items()}    
+    return pagerank_normalized
 
 
 if __name__ == "__main__":
